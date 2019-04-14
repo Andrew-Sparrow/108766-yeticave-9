91694f910e64df911edaa84c7bbb7ad6a5afb411
Andrew <8911102@gmail.com>
2019-04-14 21:12:01 +0500

    list of lots was added

--- index.php
+++ index.php
@@ -3,6 +3,45 @@ $is_auth = rand(0, 1);
 $user_name = "Андрей"; // укажите здесь ваше имя
 
 $arr_categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+
+$arr_lot = [
+  [
+    'title' => '2014 Rossignol District Snowboard',
+    'category' => 'Доски и лыжи',
+    'price' => 10999,
+    'img_src' => 'img/lot-1.jpg'
+  ],
+  [
+    'title' => 'DC Ply Mens 2016/2017 Snowboard',
+    'category' => 'Доски и лыжи',
+    'price' => 159999,
+    'img_src' => 'img/lot-2.jpg'
+  ],
+  [
+    'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+    'category' => 'Крепления',
+    'price' => 8000,
+    'img_src' => 'img/lot-3.jpg'
+  ],
+  [
+    'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+    'category' => 'Ботинки',
+    'price' => 10999,
+    'img_src' => 'img/lot-4.jpg'
+  ],
+  [
+    'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+    'category' => 'Одежда',
+    'price' => 7500,
+    'img_src' => 'img/lot-5.jpg'
+  ],
+  [
+    'title' => 'Маска Oakley Canopy',
+    'category' => 'Разное',
+    'price' => 5400,
+    'img_src' => 'img/lot-6.jpg'
+  ]
+];
 ?>
 
 <!DOCTYPE html>
@@ -75,24 +114,34 @@ $arr_categories = ["Доски и лыжи", "Крепления", "Ботинк
       </div>
       <ul class="lots__list">
         <!--заполните этот список из массива с товарами-->
-        <li class="lots__item lot">
-          <div class="lot__image">
-            <img src="#" width="350" height="260" alt="">
-          </div>
-          <div class="lot__info">
-            <span class="lot__category">Название категории</span>
-            <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-            <div class="lot__state">
-              <div class="lot__rate">
-                <span class="lot__amount">Стартовая цена</span>
-                <span class="lot__cost">цена<b class="rub">р</b></span>
-              </div>
-              <div class="lot__timer timer">
-                12:23
+        <?php foreach($arr_lot as $item): ?>
+        <?php if (isset($item)): ?>
+          <li class="lots__item lot">
+            <div class="lot__image">
+              <?php if(isset($item['img_src'])):?>
+              <img src="<?=strip_tags($item['img_src']); ?>" width="350" height="260" alt="">
+              <?php endif;?>
+            </div>
+            <div class="lot__info">
+              <?php if (isset($item['category'])): ?>
+              <span class="lot__category"><?=$item['category'] ?></span>
+              <?php endif; ?>
+              <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['title'] ?></a></h3>
+              <div class="lot__state">
+                <div class="lot__rate">
+                  <span class="lot__amount">Стартовая цена</span>
+                  <?php if(isset($item['price'])): ?>
+                  <span class="lot__cost"><?=$item['price'] ?><b class="rub">р</b></span>
+                  <?php endif;?>
+                </div>
+                <div class="lot__timer timer">
+                  12:23
+                </div>
               </div>
             </div>
-          </div>
-        </li>
+          </li>
+        <?php endif;?>
+        <?php endforeach ;?>
       </ul>
     </section>
   </main>
