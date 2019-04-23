76da53148c3f07d4666434b18b4533ce8c0d9de8
Andrew <8911102@gmail.com>
2019-04-24 00:20:57 +0500

    some indexes were deleted and types of field were changed

--- schema.sql
+++ schema.sql
@@ -11,15 +11,15 @@ create table categories (
 ) ;
 
 create table users (
-  user_id INT auto_increment primary key ,
-  dt_add TIMESTAMP  default current_timestamp,
+  id INT auto_increment primary key ,
+  dt_add DATETIME,
   email CHAR(128) not null unique ,
-  name CHAR(128) not null ,
+  name CHAR(128) not null unique ,
   password CHAR(64) not null ,
-  image TEXT,
-  contact TEXT  not null,
+  image VARCHAR(2083),
+  contact VARCHAR(2083)  not null,
   INDEX name_index (name),
-  unique INDEX email_index (email)
+  INDEX email_index(email)
 ) ;
 
 create table lots (
@@ -27,25 +27,23 @@ create table lots (
   dt_add TIMESTAMP  default current_timestamp,
   title CHAR(64) not null ,
   description TEXT not null ,
-  image TEXT not null ,
+  image VARCHAR(2083) not null ,
   start_price INT not null ,
   end_date DATE not null ,
   step SMALLINT not null,
   author_id INT unique not null , #user_id
   winner_id INT unique not null ,
   category_id INT unique not null,
-  FOREIGN KEY fk_author (author_id) REFERENCES users(user_id),
-  FOREIGN KEY fk_winner (winner_id) REFERENCES users(user_id),
+  FOREIGN KEY fk_author (author_id) REFERENCES users(id),
+  FOREIGN KEY fk_winner (winner_id) REFERENCES users(id),
   FOREIGN KEY fk_category (category_id) REFERENCES categories(id)
 ) ;
 
 create table rates (
   id INT auto_increment primary key ,
   dt_add DATETIME not null,
-  user_id INT ,
-  lot_id INT,
-  unique INDEX user_index (user_id),
-  unique INDEX lot_index (lot_id),
-  FOREIGN KEY fk_user (user_id) REFERENCES users(user_id),
+  user_id INT not null ,
+  lot_id INT not null,
+  FOREIGN KEY fk_user (user_id) REFERENCES users(id),
   FOREIGN KEY fk_lot (lot_id) REFERENCES lots(id)
 ) ;
